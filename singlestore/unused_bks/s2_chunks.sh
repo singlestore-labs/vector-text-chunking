6a2701c717aea1fe86437839e5ffdb917f64896c
Ahilya Kulkarni <akulkarni@singlestore.com>
2026-07-22 12:11:42 +0530

    Remove unused files

--- singlestore/unused_bks/s2_chunks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Wrapper script for SingleStore chunks with default password
-
-# Set your default password here
-DEFAULT_PASSWORD="root"
-
-python singlestore_chunks.py "$@" --password "$DEFAULT_PASSWORD"
